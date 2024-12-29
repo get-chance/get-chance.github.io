165058348a328062c929685274682e3ef0304b83
get-chance <50015863+get-chance@users.noreply.github.com>
2024-12-29 16:04:49 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// 팝업 열기 함수
+function openPopupForAddnDelete() {
+    document.getElementById('popup-AddnDeleteStock').style.display = 'block';
+}
+
+// 팝업 닫기 함수
+function closePopupForAddnDelete() {
+    document.getElementById('popup-AddnDeleteStock').style.display = 'none';
+}
