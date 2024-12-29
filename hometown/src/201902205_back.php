ac9cc76baf237e7189b059aa53243f731ce24b2f
get-chance <50015863+get-chance@users.noreply.github.com>
2024-12-29 16:03:41 +0900

    Delete hometown directory

--- hometown/src/201902205_back.php
+++
@@ -1,5 +0,0 @@
-<h1> 민원 접수 완료
-
-<h3> 이름 : </h3> <?php echo $_POST["username"] ?><br>
-<h3> 연락처 : </h3> <?php echo $_POST["phone"]." 또는 ".$_POST["email"]; ?><br>
-<h3> 접수된 관할 구청 :</h3> <?php echo $_POST["districts"]; ?>
